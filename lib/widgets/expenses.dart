38c1ae26106e9aaf1fa28aac29a67de9b4289644
ovaljevac <omervaljevac471@gmail.com>
2026-03-02 23:05:11 +0100

    Added the main screen and the expenses cards

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,43 @@
+import "package:expense_tracker/widgets/expenses_list/expenses_list.dart";
+import "package:expense_tracker/models/expense.dart";
+import "package:flutter/material.dart";
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: "Flutter course", amount: 19.99, date: DateTime.now(), category: Category.work),
+    Expense(title: "Cinema", amount: 15.99, date: DateTime.now(), category: Category.leisure),
+  ];
+
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => Text("modal bottom sheet"));
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter expense tracker"),
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        ],
+      )
+    );
+  }
+}
